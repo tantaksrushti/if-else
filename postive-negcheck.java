5803e6d17cbb6517f97e292d6feed44818a0ebb3
tantaksrushti <tantaksrushti@gamil.com>
2026-09-05 21:20:41 +0530

    if else java code

---
+++ postive-negcheck.java
@@ -0,0 +1,11 @@
+//Check Wether num is negative or positive using if-else Control Statment
+class IFELSE{
+	public static void main (String[] args){
+		int num1 = 5;
+		if(num1>=0){
+			System.out.println("The number is positive");
+		} else{
+			System.out.println("The number is negative");
+		}
+	}
+}
