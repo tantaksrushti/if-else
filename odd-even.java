1844e118d7c762cf8725f074133e13c7b2ac1c43
tantaksrushti <tantaksrushti@gamil.com>
2026-09-05 21:45:21 +0530

    if-else java code

---
+++ odd-even.java
@@ -0,0 +1,15 @@
+import java.util.*; // import util 
+class EVOD{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);// connects to keybord  
+	        
+		System.out.println("Enter a number: ");
+		int num = sc.nextInt();
+
+		if(num%2==0){
+			System.out.println("The number is even");
+		} else{
+			System.out.println("The number is odd");
+		}
+	}
+}
