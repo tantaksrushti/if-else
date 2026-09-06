9fd85d2dd9bc1ce6d3b4126bdb6c6e7fa580c81a
tantaksrushti <tantaksrushti@gamil.com>
2026-09-06 12:03:13 +0530

    if else-if else java code

---
+++ Month.java
@@ -0,0 +1,40 @@
+//To print the month name according to the month number
+import java.util.Scanner;
+class MonthChecker{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter number between 1 to 12): ");
+		int num = sc.nextInt();
+
+		System.out.println(num);
+
+		if(num==1){
+			System.out.println("January");
+		}else if(num==2){
+                        System.out.println("February");
+                }else if(num==3){
+                        System.out.println("March");
+                }else if(num==4){
+                        System.out.println("April");
+                }else if(num==5){
+                        System.out.println("May");
+                }else if(num==6){
+                        System.out.println("June");
+                }else if(num==7){
+                        System.out.println("July");
+                }else if(num==8){
+                        System.out.println("August");
+                }else if(num==9){
+                        System.out.println("September");
+                }else if(num==10){
+                        System.out.println("October");
+                }else if(num==11){
+                        System.out.println("November");
+                }else if(num==12){
+                        System.out.println("December");
+                }else{
+			System.out.println("Invalid input for month");
+		}
+	}
+}
